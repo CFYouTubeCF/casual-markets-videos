7ef824525fa5abe5b440419b4b1243f61d494fe5
CFYouTubeCF <contact.casualfinance@gmail.com>
2026-04-06 18:29:21 -0500

    Update fetch-videos.js

--- fetch-videos.js
+++ fetch-videos.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 const API_KEY = process.env.YOUTUBE_API_KEY;
 const CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID;
 
-const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet&order=date&maxResults=5&type=video`;
+const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet&order=date&maxResults=10&type=video&videoDuration=medium`;
 
 https.get(url, (res) => {
   let data = '';
   res.on('data', chunk => data += chunk);
   res.on('end', () => {
     const raw = JSON.parse(data);
-    const videos = raw.items.map(item => ({
+    const videos = raw.items.slice(0, 5).map(item => ({
       id: item.id.videoId,
       title: item.snippet.title,
       description: item.snippet.description,
